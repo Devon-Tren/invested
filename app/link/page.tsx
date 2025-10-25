eb44d78a4582e0a3be348b7c186bcbc88c7c8cbb
hector1128 <hectorhoraciocordero11@gmail.com>
2025-10-25 19:33:31 -0400

    structure of page

---
+++ app/link/page.tsx
@@ -0,0 +1,7 @@
+export default function Link() {
+  return (
+    <div className="min-h-screen bg-[#0e1a2a] text-slate-200 flex items-center justify-center">
+      <h1 className="text-4xl font-extrabold">Link Page Coming Soon!</h1>
+    </div>
+  );
+}
